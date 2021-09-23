b4c3131ed609ed3184df8fa4d90370d293378826
TLundComputing <33316738+TLundComputing@users.noreply.github.com>
2021-09-23 10:54:32 +0100

    Adding starter code for arithmetic test

---
+++ Programming Techniques/src/challenges/ArithmeticTest.java
@@ -0,0 +1,18 @@
+package challenges;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ArithmeticTest {
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int question = 1;
+        Random generator = new Random();
+        while(question < 11){
+
+        }
+
+    }
+
+}
